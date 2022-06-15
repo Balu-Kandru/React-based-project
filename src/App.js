2de9ccf82b8a2a3da2252e91da88ca6c46d9354c
Balu_Kandru <swamy.kandru@gmail.com>
2022-06-15 15:47:46 +0530

    fully completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import { Fragment } from 'react';
 import './App.css';
 import Postview from './components/PostView';
+import Landing from './components/LandingPage';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return(
-    <Fragment>
-      <Postview />
-    </Fragment>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Landing}></Route>
+        <Route path='/post' component={Postview}></Route>
+      </Switch>
+    </BrowserRouter>
     
   )
 }
---
+++ src/components/LandingPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Landing(){
+    return(
+        <>
+        <div className="Landing">
+            <img className="landing_img" src="https://images.unsplash.com/photo-1655033116463-edffb5c38648?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDIxfGJvOGpRS1RhRTBZfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=700&q=60" alt="" />
+            <div className="name_button">
+                <h2 className="name">10x Team 04</h2>
+                <Link className="button" to='/post'>Enter</Link>
+            </div>
+        </div>
+        </>
+    )
+}
+export default Landing;
